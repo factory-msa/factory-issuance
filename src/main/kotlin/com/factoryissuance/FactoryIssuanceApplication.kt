3b73da15a943f698d84585a91be39000430752dc
juhyun <zzang9ha@naver.com>
2023-10-07 16:16:45 +0900

    initial commit

---
+++ src/main/kotlin/com/factoryissuance/FactoryIssuanceApplication.kt
@@ -0,0 +1,11 @@
+package com.factoryissuance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FactoryIssuanceApplication
+
+fun main(args: Array<String>) {
+    runApplication<FactoryIssuanceApplication>(*args)
+}
---
+++ src/test/kotlin/com/factoryissuance/FactoryIssuanceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.factoryissuance
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FactoryIssuanceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
