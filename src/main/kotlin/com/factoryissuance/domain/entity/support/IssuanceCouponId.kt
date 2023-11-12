561aa59bf5bafbe6aad4043af73d7122dc8dc40b
juhyun <zzang9ha@naver.com>
2023-11-12 22:39:19 +0900

    refactor: remove not used @Id annotation

--- src/main/kotlin/com/factoryissuance/domain/entity/support/IssuanceCouponId.kt
+++ src/main/kotlin/com/factoryissuance/domain/entity/support/IssuanceCouponId.kt
@@ -3,16 +3,13 @@ package com.factoryissuance.domain.entity.support
 import java.io.Serializable
 import javax.persistence.Column
 import javax.persistence.Embeddable
-import javax.persistence.Id
 
 @Embeddable
 class IssuanceCouponId(
 
-    @Id
     @Column(name = "ISSUANCE_COUPON_ID", nullable = false)
     var issuanceCouponId: String,
 
-    @Id
     @Column(name = "ISSUANCE_REQUEST_ID", nullable = false)
     var issuanceRequestId: String,
 ) : Serializable
