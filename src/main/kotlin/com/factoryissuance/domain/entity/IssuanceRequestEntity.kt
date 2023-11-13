dc767f7b02db344c8326f5859c8e5cc0bccb55f0
juhyun <zzang9ha@naver.com>
2023-11-14 00:25:24 +0900

    refactor: ISSUANCE_REQUEST Entity id's strategy change to AUTO_INCREMENT

--- src/main/kotlin/com/factoryissuance/domain/entity/IssuanceRequestEntity.kt
+++ src/main/kotlin/com/factoryissuance/domain/entity/IssuanceRequestEntity.kt
@@ -6,16 +6,14 @@ import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.EnumType
 import javax.persistence.Enumerated
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.Table
 
 @Entity
 @Table(name = "ISSUANCE_REQUEST")
 class IssuanceRequestEntity(
-
-    @Id
-    @Column(name = "ISSUANCE_REQUEST_ID", nullable = false)
-    var issuanceRequestId: String,
 
     @Column(name = "TRANSACTION_ID", nullable = false)
     var transactionId: String,
@@ -24,14 +22,17 @@ class IssuanceRequestEntity(
     @Column(name = "ISSUANCE_REQUEST_CODE", nullable = false)
     var issuanceRequestCode: IssuanceRequestCode,
 
-    @Column(name = "ISSUANCE_RESULT_CODE")
-    var issuanceResultCode: String,
-
     @Column(name = "PRICE", nullable = false)
     var price: Long,
 
     @Column(name = "ISSUANCE_QUANTITY", nullable = false)
-    var quantity: Int
+    var quantity: Int,
+
+    @Id
+    @Column(name = "ISSUANCE_REQUEST_ID", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var issuanceRequestId: Long? = null
+
 ) : BaseTimeEntity() {
 
 }
