483e9cc48655aaf6cdda54c32b9ca267ac79be21
juhyun <zzang9ha@naver.com>
2023-11-25 20:16:05 +0900

    refactor: change api uri (gateway call service name)

--- src/main/kotlin/com/factoryissuance/api/v1/IssuanceController.kt
+++ src/main/kotlin/com/factoryissuance/api/v1/IssuanceController.kt
@@ -1,19 +1,18 @@
 package com.factoryissuance.api.v1
 
 import com.factoryissuance.api.dto.ApiResponse
+import com.factoryissuance.application.IssuanceFacadeService
 import com.factoryissuance.application.IssuanceService
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("/api/v1")
 @RestController
 class IssuanceController(
-    private val service: IssuanceService
+    private val service: IssuanceFacadeService
 ) {
 
-    @PostMapping("coupons/issue")
+    @PostMapping("/api/v1/issuance/coupons/issue")
     fun issueCoupons(
         @RequestBody command: IssuanceService.CouponIssuanceCommand
     ): ApiResponse<IssuanceService.CouponIssuanceResponse> {
