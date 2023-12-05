c89b1be6e0a59c6e2f0936aeca3e3034d128da0e
juhyun <zzang9ha@naver.com>
2023-12-05 23:24:03 +0900

    refactor: change IssuanceCouponEntity id field

--- src/main/kotlin/com/factoryissuance/domain/entity/IssuanceCouponEntity.kt
+++ src/main/kotlin/com/factoryissuance/domain/entity/IssuanceCouponEntity.kt
@@ -1,29 +1,25 @@
 package com.factoryissuance.domain.entity
 
-import com.factoryissuance.domain.entity.support.IssuanceCouponId
-import org.springframework.data.domain.Persistable
 import support.domain.BaseTimeEntity
 import javax.persistence.Column
-import javax.persistence.EmbeddedId
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
 import javax.persistence.Table
 
 @Entity
 @Table(name = "ISSUANCE_COUPON")
 class IssuanceCouponEntity(
 
-    @EmbeddedId
-    var issuanceCouponId: IssuanceCouponId,
+    @Column(name = "ISSUANCE_REQUEST_ID", nullable = false)
+    var issuanceRequestId: String, // globalTransactionId,
 
     @Column(name = "COUPON_NUMBER", nullable = false)
     var couponNumber: String,
-) : BaseTimeEntity(), Persistable<IssuanceCouponId> {
 
-    @Transient
-    private var isNew: Boolean = true
-
-    override fun getId(): IssuanceCouponId? = issuanceCouponId
-
-    override fun isNew(): Boolean = isNew
-
-}
+    @Id
+    @Column(name = "ID", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+) : BaseTimeEntity()
